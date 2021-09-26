a5dcf6dc92aed3bac7a97efef07c92edb2ac4a7f
Ji <csy960407@naver.com>
2021-09-27 04:47:58 +0900

    식단 등록 진행중 (권한등록..)

--- app/src/main/java/com/example/kotlinmessenger/AddPhotoActivity.kt
+++ app/src/main/java/com/example/kotlinmessenger/AddPhotoActivity.kt
@@ -1,11 +1,18 @@
 package com.example.kotlinmessenger
 
 import android.Manifest
+import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.drawable.BitmapDrawable
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.view.MenuItem
+import android.view.View
+import android.widget.PopupMenu
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -48,14 +55,23 @@ class AddPhotoActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if(requestCode==PICK_IMAGE_FROM_ALBUM){
-            if(resultCode==PICK_IMAGE_FROM_ALBUM){
+            if(resultCode== Activity.RESULT_OK){
                 photoUri=data?.data
                 addphoto_image.setImageURI(photoUri)
+                val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, photoUri)
+                val bitmapDrawable = BitmapDrawable(bitmap)
+                addphoto_image.setBackgroundDrawable(bitmapDrawable)
+                Log.d(this.toString(),"시발");
             }else{
+                Log.d(this.toString(),"개시발");
                 finish();
             }
         }
     }
+
+
+
+
 
     fun contentUpload(){
         var timestamp=SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
